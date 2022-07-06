bef1c63fa81b3f40d51cdd3bb8c052bea6439c7b
linahello <tropikalmoon@gmail.com>
2022-07-06 16:37:25 +0200

    say_hello

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,3 +1,8 @@
 # un programme qui dit Bonjour pour Vald
 
-puts "Bonjour poto !"
+def say_hello #fonction role affiche "Bonjour poto!"
+  puts "Bonjour poto !"
+end 
+
+say_hello # pour appeller la fonction
+
