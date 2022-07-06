2c62f39dc82efd747fe9cb6575fd6d1e8274eb96
linahello <tropikalmoon@gmail.com>
2022-07-06 21:39:02 +0200

    exos 2 & 3

---
+++ lib/03_stairway.rb
@@ -0,0 +1,12 @@
+# monter 10 marches en fonction d'un jet de dé
+# à l'éxécution, prgrm lance une partie :
+# player bottom 10 steps stairs, à chaque tour il lance le dé
+# if 5 || 6 : avance d'1 marche et le progr lui dit ainsi que la marche où il est à présent
+# s'il fait 1, il descend d'1 marche et le progr dit et la marche
+# s'il fait 2, 3 ou 4 rien ne se passe, le progr lui dit ainsi que la marche où il est resté
+
+
+
+
+
+
